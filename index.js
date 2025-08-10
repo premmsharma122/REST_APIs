5d5a29cc7bdd87a3a50350f449cfd9833b44bc18
Prem Sharma <sonusharma4237@gmail.com>
2025-08-10 11:59:14 +0530

    Updated index

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require("express")
 const app = express()
 const path = require("path")
 const {v4: uuidv4} = require("uuid")
+const methodOverride = require("method-override")
 const port = 3000;
 
 app.use(express.urlencoded({extended: true}))
-
+app.use(methodOverride('_method'))
 app.set("view engine", "ejs")
 app.set("views", path.join(__dirname, "views"))
 app.use(express.static(path.join(__dirname, "public")))
@@ -57,6 +58,27 @@ app.get("/posts/:id", (req,res)=>{
    res.render("show.ejs", {post})
    
 })
+app.patch("/posts/:id", (req,res)=>{
+    let {id} = req.params
+    let newContent = req.body.content
+    let post = posts.find((p)=> p.id === id)
+    post.content = newContent
+    res.send("Post Updated!")
+    
+})
+app.get("/posts/:id/edit", (req,res)=>{
+    let {id} = req.params
+    let post = posts.find((p)=> p.id === id)
+    res.render("edit.ejs", {post})
+    
+})
+app.delete("/posts/:id",(req,res)=>{
+    let {id} = req.params
+    posts = posts.filter((p)=> p.id !== id)
+    res.send("Delete Post!")
+    res.redirect("/posts")
+})
+
 app.get("/", (req,res)=>{
     res.send(`Server running at ${port}`)
 })
